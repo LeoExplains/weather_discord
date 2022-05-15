b72a47d8ba28320d54e77c78360eefc84b49188f
Leo <87410274+LeoExplains@users.noreply.github.com>
2022-05-15 06:38:10 +0000

    updated uv and temp check output

--- check.py
+++ check.py
@@ -1,18 +1,65 @@
 ## Helper functions for checking temperature
-
+from api import *
 # Check min & max temp
-def 
-# Check humidity
+def temp_change(obj):
+  feels_temp = {
+    "morning": obj.feels_like_morn, 
+    "daytime": obj.feels_like_day, 
+    "evening": obj.feels_like_eve, 
+    "night": obj.feels_like_night
+  }
+  # printing out the values 
+  cold=(f'The coldest part of the day will be in the {min(feels_temp.keys())} with a temp of {min(feels_temp.values())}\u00B0c')
+  hot=(f'The warmest part of the day will be in the {max(feels_temp.keys())} with a temp of {max(feels_temp.values())}\u00B0c')
+  change = ""
+  if (obj.temp_day - obj.temp_morn) > 3:
+    change = (f"Morning > Afternoon will get warmer by {obj.temp_day - obj.temp_morn:.2f}\u00B0c")
+  elif (obj.temp_eve - obj.temp_day) > 3:
+    change = (f"Afternoon > Evening will get warmer by {obj.temp_day - obj.temp_morn:.2f}\u00B0c")
+  elif (obj.temp_day - obj.temp_morn) < -2:
+    change = (f"Morning > Afternoon will get colder by {obj.temp_day - obj.temp_morn:.2f}\u00B0c")
+  elif (obj.temp_day - obj.temp_morn) < -2:
+    change = (f"Afternoon > Evening will get colder by {obj.temp_day - obj.temp_morn:.2f}\u00B0c")
+  else: 
+    change = (f"It's going to stay mostly the same temperature during the day")
+  return (cold, hot, change)
 
+# check uv index
+def uv_recommendation(obj):
+  """Returns true for sun protection and or Stay inside"""
+# UV Index scale
+# low (1-2)
+# moderate (3-5)
+# high (6-7)
+# very high (8-10)
+# extreme (11 and above).
+  sunprotect = ""
+  if obj.uvi >= 3:
+    sunprotect = (f"You should wear suncreen or a hat today. It's going to be a UV Index of: {obj.uvi:.1f}")
+  elif obj.uvi >= 8:
+    sunprotect = (f"You should stay inside today. If you have to go out bring sunscreen. It's going to be a UV Index of: {obj.uvi:.1}")
+  return (sunprotect)
+  
+# Check humidity
+# need to check more about dew point vs humidity for accuracy. 
+
+# check wind gust & speed
+# need more information about wind gust and speed to feeling
+  
 # check rainfall
+def rain_check(obj):
+  rain_protect = ""
+  if (obj.chance_of_rain * 100) > 40:
+    rain_protect = (f"You should bring an umbrella today. It's {obj.chance_of_rain:.02%} likely to rain today")
+  return (rain_protect)
+
+  clothes = ""
+  sunscreen = ""
+  rain = ""
+  inside = ""
 
 #check description
 
-# check uv index
-
-# Check Air Quality
-
-# check wind gust
 
 # check change in day
 
--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from datetime import datetime, timedelta
+from api import *
+from check import *
 dt = datetime.now()
 td = timedelta(days=7)
 # your calculated date
@@ -9,16 +11,15 @@ my_date = dt + td
 print(f"Hello I can forcast up to {my_date} for you")
 # Display Results
 
-from api import *
-location = get_lat_lon("hughesdale")
+location = get_lat_lon("cremorne")
 try:
    city = (location[0], location[1], location[2])
    weather = get_current_weather(location[3], location[4])
    set_days_weather(weather, city)
-  
-   print(today.msg())
-   print(tomorrow.msg())
 except Exception:
    print("location not provided")
 
+print(temp_change(today))
+print(uv_recommendation(today))
+print(rain_check(today))
 # print(get_pollution(location[0], location[1]))
