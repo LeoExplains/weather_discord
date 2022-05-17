015624389775a08cf7e258ca0a72c0d198a81af3
Leo <87410274+LeoExplains@users.noreply.github.com>
2022-05-17 00:25:59 +0000

    added tomorrow option

--- main.py
+++ main.py
@@ -72,6 +72,58 @@ class MyClient(discord.Client):
           set_user_location(user_location)
           await message.channel.send(short_chat(today))
 
+        if message.content.startswith('!tomorrow'):
+          await message.reply('Enter a location:')
+          input = await self.wait_for('message')
+          result = get_lat_lon(input.content)
+          await message.channel.send("Please wait while I look up locations matching that")
+          for i, j in enumerate(result):
+            option = f"{i + 1}: {j}"
+            await message.channel.send(option)
+
+          await message.channel.send("Select a location number:")  
+          input = await self.wait_for('message')
+          # would like to consolidate this below into a cleaner construct
+          
+          if int(input.content) == 1:
+            await message.channel.send(result[0])
+            user_location = result[0]
+          elif int(input.content) == 2:
+            await message.channel.send(result[1])
+            user_location = result[1]
+          elif int(input.content) == 3:
+            await message.channel.send(result[1])
+            user_location = result[2]
+          elif int(input.content) == 4:
+            await message.channel.send(result[1])
+            user_location = result[3]
+          elif int(input.content) == 5:
+            await message.channel.send(result[1])
+            user_location = result[4]
+          elif int(input.content) == 6:
+            await message.channel.send(result[1])
+            user_location = result[5]
+          elif int(input.content) == 7:
+            await message.channel.send(result[1])
+            user_location = result[6]
+          elif int(input.content) == 8:
+            await message.channel.send(result[1])
+            user_location = result[7]
+          elif int(input.content) == 9:
+            await message.channel.send(result[1])
+            user_location = result[8]
+          elif int(input.content) == 10:
+            await message.channel.send(result[1])
+            user_location = result[9]
+          else: 
+            await message.channel.send(type(input.content))
+            await message.channel.send(input.content)
+              
+            
+          # still need to display all location options and confirm with user.
+          set_user_location(user_location)
+          await message.channel.send(short_chat(tomorrow))
+
 
 intents = discord.Intents(messages=True)
 
