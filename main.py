e35ac0126f3fb7bcb651d53343bb0a9c1becc248
Leo <87410274+LeoExplains@users.noreply.github.com>
2022-05-17 00:04:15 +0000

    got the location selection working

--- main.py
+++ main.py
@@ -24,18 +24,27 @@ class MyClient(discord.Client):
           await message.reply('Set a location:')
           input = await self.wait_for('message')
           result = get_lat_lon(input.content)
+          await message.channel.send("select a location number")
           for i, j in enumerate(result):
-            option = i + 1, j
+            option = f"{i + 1}: {j}"
+            #await message.channel.send("Is this the location? (y/n)")
             await message.channel.send(option)
+            
+          input = await self.wait_for('message')
+          if int(input.content) == 1:
+              await message.channel.send(result[0])
+          else: 
+            await message.channel.send(type(input.content))
+            await message.channel.send(input.content)
+              
             def is_choice(m):
               return m.author == message.author and m.content.isdigit()
 
-            try:
-                  input = await self.wait_for('message', check=is_choice, timeout=15.0)
-            except asyncio.TimeoutError: # fix this to be defined
-                  return await message.channel.send(f'Sorry, you took too long, defaulting to Melbourne Vic AU.')
-            if int(input.content) == "y":
-                await message.channel.send(j)
+            # try:
+            #       input = await self.wait_for('message', check=is_choice, timeout=15.0)
+            # except asyncio.TimeoutError: # fix this to be defined
+            #       return await message.channel.send(f'Sorry, you took too long, defaulting to Melbourne Vic AU.')
+            
             
             # still need to display all location options and confirm with user.
           # await message.channel.send(set_user_location(get_lat_lon(input.content)))
